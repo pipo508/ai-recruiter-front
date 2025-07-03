2d6ef73e63c5a2049e06d1c6616201440a8cb6b6
pipo508 <j.verdini@alumno.um.edu.ar>
2025-07-03 03:10:55 -0300

    arreglo env en api

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@
 
 
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 // Construimos las URLs completas
 export const API_DOCUMENT_URL = `${API_BASE_URL}/api/document`; // para PDFs
